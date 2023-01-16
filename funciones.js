12d23be2dfedbcdb412f93a63b9ce32f7f9e4767
Martin <martinalvaro3175@gmail.com>
2023-01-16 13:32:22 -0300

    empezando el responsive

---
+++ funciones.js
@@ -0,0 +1,54 @@
+ const datos = '{"edad":25, "pais":"Estados Unidos", "email": "eugene.crawford@example.com"}'
+ const obj = JSON.parse(datos)
+
+let readMore_btn = document.getElementById('readMore_btn');
+let hideText = document.getElementById('hideText');
+
+readMore_btn.addEventListener('click', toggleText);
+
+function toggleText() {
+  hideText.classList.toggle('showText');
+  document.getElementById('dato1').innerHTML= obj.edad;
+  document.getElementById('dato2').innerHTML= obj.pais;
+  document.getElementById('dato3').innerHTML= obj.email;
+  if(hideText.classList.contains('showText')) {
+    readMore_btn.innerHTML = 'Menos info'
+  }
+  else {
+    readMore_btn.innerHTML = 'Mas info'
+  }
+}
+
+
+  function obtenerDatos(){
+    fetch('habilidades.json')
+      .then((resp) => resp.json() )
+      .then((data)=>{
+      // console.log(data)
+      data.forEach(hab => {
+        document.getElementById("hab").innerHTML+=`
+        <article>
+          <div>
+            <div>
+              <img src="${hab.img}"</img>
+            </div>     
+              <h3>${hab.titulo}</h3>
+          </div>  
+          <p>${hab.conocimiento}</p>
+        </article>`
+      });
+    })
+
+  }
+
+  obtenerDatos()
+
+
+  function menuResponsive(){
+    let x = document.getElementById("nav")
+    if(x.className==""){
+      x.className = "responsive"
+    } else{
+      x.className = ""
+    }
+  }
